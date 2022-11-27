c36df875815326083f11b8165b0a8d75bb1c6a10
Ebenezer <akhonyaebenezer@gmail.com>
2022-11-27 16:42:49 +0300

    Added posts route

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return ("Hi Mom! This is home")
+    users = {
+        "name":"John Doe",
+        "posts": 13
+    }
+    return (render_template('index.html', title="Home", user=users))
 
+
+@app.route('/posts')
+def posts():
+    posts = {
+        "id":"4354",
+        "title":"Getting good at git",
+        "text":"Diving deep into git and VCS",
+        "author":"John Doe",
+        "time_creation":"17-10-2022"
+    }
+    return(render_template('posts.html', posts=posts))
